7a1a03125983a44d8a5651cdd80f00cf0c42e7f4
asharlaimov <sgnllsa@gmail.com>
2014-04-04 23:47:15 +0300

    add Category

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401102042) do
+ActiveRecord::Schema.define(version: 20140404203135) do
 
   create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
