63643c92483a3c4c33f35ad6eae4174b60536f5e
Aleksey <asharlaimov@gmail.com>
2014-04-17 00:03:16 +0300

    cache improvement

--- app/models/recommended.rb
+++ app/models/recommended.rb
@@ -1,5 +1,9 @@
 class Recommended < ActiveRecord::Base
-  belongs_to :product
+  belongs_to :product, touch: true
   validates :product, presence: true, uniqueness: true
   validates :position, presence: true
+
+  def self.latest
+    Recommended.order(:updated_at).last
+  end
 end
