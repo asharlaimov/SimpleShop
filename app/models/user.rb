f5473c9f8723bea16ec44d5de7a6bed4d814d3d4
Aleksey <asharlaimov@gmail.com>
2014-04-25 21:40:59 +0300

    fix creation of new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   after_destroy :ensure_an_admin_remains
+  validates :name, presence: true, uniqueness: true
 
   private
   def ensure_an_admin_remains
     if User.count.zero?
-      raise "Последний пользователь не может быть удален"
+      raise "The last Admin can not de deleted"
     end
   end
 end
